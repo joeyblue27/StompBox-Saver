77eb2e5c220a14e41c08ae2e9a6bfc1a3df013f7
Joey Douek <joeyblue27@gmail.com>
2023-04-10 23:50:37 -0400

    models and typedefs

---
+++ server/models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must use a valid email address"],
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  pedalsettings: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "PedalState",
+    },
+  ],
+});
+
+// set up pre-save middleware to create password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+
+userSchema.statics.findByUserId = async function (userId) {
+  const user = await this.findOne({ _id: userId }).populate("pedalsettings");
+  return user;
+};
+
+const User = model("User", userSchema);
+module.exports = User;
---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const PedalState = require("./PedalState");
+const User = require("./User");
+
+module.exports = { PedalState, User };
